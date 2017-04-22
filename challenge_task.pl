679e012982e2abaeb83760ff27777736f4e117c9
Khaled Q. Abdelfadeel <kqorany2@gmail.com>
2017-04-22 14:50:17 +0100

    Read the network file
    Read the netwrok file and split the names in each line using the comma

--- challenge_task.pl
+++ challenge_task.pl
@@ -8,3 +8,32 @@ use diagnostics;  # The diagnostics pragma
 #  distance between two members of the network            #	
 ###########################################################
 
+# The number of people in the network
+my $num_people;
+# A Hash to save people
+my %people;
+# A Hash to save the friendships
+my %friendships;
+
+# Open the social network file
+my $network_file = $ARGV[0];
+open my $fh,'<',$network_file 
+	or die "Can't open File : $_";
+	
+# Check the format of the file
+
+# Read the file
+while(my $info = <$fh>){
+	chomp($info	);
+	my ($friend_1,$friend_2) = split /,/,$info;
+	foreach my $friend ($friend_1,$friend_2){
+	
+	
+	}
+
+
+
+}
+
+close $fh 
+	or die "Couldn't close File : $_";
