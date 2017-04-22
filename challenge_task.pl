6f215d6c9fa6a3292dd0f17df87ebd5df0e9466b
Khaled Q. Abdelfadeel <kqorany2@gmail.com>
2017-04-22 14:56:37 +0100

    Find the total number of people in the network

--- challenge_task.pl
+++ challenge_task.pl
@@ -26,14 +26,24 @@ open my $fh,'<',$network_file
 while(my $info = <$fh>){
 	chomp($info	);
 	my ($friend_1,$friend_2) = split /,/,$info;
-	foreach my $friend ($friend_1,$friend_2){
-	
-	
+	foreach my $person ($friend_1,$friend_2){
+		if(exists $people{$person}){
+			# This person is already added
+			print "$pearson is exist in the netwrok before with value $people{$person}\n";
+		
+		} else {
+			print "$person is not exist in the network\n";
+			# Add this person
+			$people{$person}=$num_people++;
+			$friendships{$person}=\my @temp;
+			print "$person is taking the value $people{$person}\n";
+		}
 	}
-
-
-
+	# Add this friendship
+	push @{$friendships{$friend_1}}, $friend_2;
+	push @{$friendships{$friend_2}}, $friend_1;
 }
-
 close $fh 
 	or die "Couldn't close File : $_";
+	
+print "The number of people in the network is ($num_people)\n";
