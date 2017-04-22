1496b8de38d748fe192314cbdf86c4767e11cb01
Khaled Q. Abdelfadeel <kqorany2@gmail.com>
2017-04-22 14:44:40 +0100

    Initiate the task

---
+++ challenge_task.pl
@@ -0,0 +1,10 @@
+use strict;       # The strict pragma
+use warnings;     # The warnings pragma
+use diagnostics;  # The diagnostics pragma
+
+###########################################################
+#  This program determines the total number of people in  #
+#  a social network, in addition, determining the minimum #
+#  distance between two members of the network            #	
+###########################################################
+
